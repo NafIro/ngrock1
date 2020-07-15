749450fbf8359c42c476112f0e5b30d1992d0894
Nafissa Iro <nafissairo@hotmail.com>
2020-07-15 15:42:09 -0400

    test4 aurevoir ngrock

--- JavaApplication4/src/javaapplication4/JavaApplication4.java
+++ JavaApplication4/src/javaapplication4/JavaApplication4.java
@@ -18,6 +18,7 @@ public class JavaApplication4 {
         // TODO code application logic here
         System.out.println("Bonjour "+args[1]+", " +args[0]);
         System.out.println("Au revoir trés cher!");
+        System.out.println("Au revoir trés cher!");
     }
     
 }
